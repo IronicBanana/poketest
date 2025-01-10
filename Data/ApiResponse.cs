dd58e827c890d5043a31115c81f3077f1dc142db
IronicBanana <ironicbanana1113@gmail.com>
2025-01-10 14:07:45 -0800

    first full commit, base template

---
+++ Data/ApiResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace poketest.Data
+{
+    public class ApiResponse
+    {
+          
+        public List<Pokemon> Data { get; set; }
+    }
+
+}
---
+++ Data/pokeClass.cs
@@ -0,0 +1,12 @@
+﻿namespace poketest.Data
+
+{
+    public class Pokemon
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+
+    }
+
+
+}
